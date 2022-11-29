4290c2aa986bdfc4e4b4f9ba0bba4d694420f496
Krishnapriya Amarnath <priya.amarnath23@gmail.com>
2022-11-28 22:04:40 -0800

    added icons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { MdLocationOn } from 'react-icons/fa';
 
 
 export default function Card (props) {
     const img = props.item.imageUrl;
     return (
         <div className="cards-section">
-
             <div className="card--container">
                 <div className="img-container">
                     <img className="card-img" src={img} alt="mount fuji" />
                 </div>   
                     <div className="card--content--container" >
                         <div className="card--title--content">
+                                <MdLocationOn/> 
                                 <h3>{props.item.location}</h3>
                                 <a href={props.item.googleMapsUrl}>view on google maps</a>
                         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
+import { FaGlobeEurope } from 'react-icons/fa';
 
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-           
+            <FaGlobeEurope/>
             <p> My travel Journal </p>
         </nav>
     )
