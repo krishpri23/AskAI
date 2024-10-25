c46efeeb48d5a4378a02ead01ef7fc284eb9b6a4
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-10-25 15:39:52 -0700

    minor fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,12 @@ import "./index.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage.jsx";
 import DashboardPage from "./pages/DashboardPage/DashboardPage.jsx";
-import ChatPage from "./pages/chatpage/ChatPage.jsx";
 import RootLayout from "./layouts/RootLayout/RootLayout.jsx";
 import DashboardLayout from "./layouts/DashboardLayout/DashboardLayout.jsx";
 import SignInPage from "./pages/SignInPage/SignInPage";
 import SignupPage from "./pages/SignUpPage/SignupPage";
+import ChatPage from "./pages/chatPage/Chatpage.jsx";
+
 
 const router = createBrowserRouter([
   {
